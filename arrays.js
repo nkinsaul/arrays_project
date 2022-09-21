58d9621232adb23349fa5bc54ed8e17350b2ed9b
Natalie Kinsaul <n.kinsaul@gmail.com>
2022-09-21 11:10:30 -0700

    Finish arrays challenge

--- arrays.js
+++ arrays.js
@@ -1,3 +1,16 @@
 var friends = ["Danny", "Doug", "Sarah", "Richard"];
 var nums = [5, 16, 2, 5];
 var bools = [true, false, true, false];
+
+//This will isolate the last element in the array.  It will
+//then print the last element followed by the array with
+//the last element removed
+console.log(friends.pop());
+console.log(friends);
+
+//This will print the elements in the array at index positions 0-1
+console.log(nums.slice(0, 2));
+
+//This will add a true to the end of the array and then output the
+//new number of elements in the array
+console.log(bools.push(true));
